8a27ef7cdbdea66462f952417fec81c6493bb364
malex98 <malex98@mail.ru>
2016-12-19 16:04:02 +0300

    project created

--- dchoosetovar.h
+++ dchoosetovar.h
@@ -14,7 +14,7 @@ class dChooseTovar : public QDialog
 public:
     explicit dChooseTovar(QWidget *parent = 0);
     ~dChooseTovar();
-    void addTovar(QString str, int i, QIcon icon);
+    void addTovar(QString str, int i, QIcon icon);//добавляет строку в QCombobox, id, и иконку
     int count();
     int result();
     void clearcbNaim();
